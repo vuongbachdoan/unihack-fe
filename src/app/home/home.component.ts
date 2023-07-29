a9a43cf0ea997dccccf13b6d11f7e1f2a80d3805
ngxuancam <nguyenxuancamit@gmail.com>
2023-07-30 01:53:49 +0700

    [Home] Update style left menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,27 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   navigators = [
     {
       id: 1,
       name: "Dashboard",
-      svgPath: "/assets/images/dashboard.svg",
+      svgPath: "../../assets/icons/dashboard.svg",
       path: "/dashboard"
     },
     {
       id: 2,
       name: "Cost",
-      svgPath: "/assets/images/cost.svg",
+      svgPath: "../../assets/icons/cost.svg",
       path: "/cost"
     },
     {
       id: 5,
       name: "Devices",
-      svgPath: "/assets/images/emissions.svg",
+      svgPath: "../../assets/icons/emissions.svg",
       path: "/devices"
+    },
+    {
+      id: 6,
+      name: "Plans",
+      svgPath: "../../assets/icons/emissions.svg",
+      path: "/plans"
     }
   ]
 }
