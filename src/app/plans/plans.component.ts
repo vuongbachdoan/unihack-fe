955a78a9c6713e5fab412e77b0c839e6462f02c2
ngxuancam <nguyenxuancamit@gmail.com>
2023-07-30 02:54:06 +0700

    [Plans] Fetch Data

--- src/app/plans/plans.component.ts
+++ src/app/plans/plans.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-plans',
   templateUrl: './plans.component.html',
-  styleUrls: ['./plans.component.scss']
+  styleUrls: ['./plans.component.scss'],
 })
 export class PlansComponent {
+  dataListFetch;
+  isFetch: boolean = true;
+
+  constructor (db: AngularFireDatabase) {
+
+    this.dataListFetch = db.list('DeviceList').valueChanges()
+
+  }
 
 }
